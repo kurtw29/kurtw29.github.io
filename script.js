e768f2de9b60eabe081cea44199c1225fd45372e
kurtw29 <ckurt37@gmail.com>
2018-12-19 16:00:31 -0800

    clickable wip

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-var hello = document.getElementById("hello");
-var greet = function(){
-    alert("Hello World!");
-};
-hello.addEventListener("click", greet());
+$(".project-container").hover(
+    function(){
+        $(this).children(".project-name").css("margin-top", "5%");
+        $(this).children(".project-tags").css("display", "flex");
+    },
+    function(){
+        $(this).children(".project-name").css("margin-top", "25%");
+        $(this).children(".project-tags").css("display", "none");
+    }
+)
+
+
+//// skill chart with "chartjs"
+// var ctx = document.getElementById("skills-chart").getContext('2d');
+// var skillChart = new Chart(ctx, {
+//     type: 'horizontalBar',
+//     data: {
+//         labels: ["Angular6","Java","Python","JavaScript", "HTML/CSS"],
+//         datasets: [{
+//             label: "Technologies",
+//             backgroundColor: ['rgb(255,99,132)'],
+//             borderColor: ['rgb(255,99,132)'],
+//             data: [10,20,30,40,50],
+//         }]
+//     },
+//     options: {}
+// });
